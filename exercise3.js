8c7347577612896a5466ddb3be0a73db2e00fcc9
aliamarsha <aliamarsha96@gmail.com>
2019-11-21 20:05:08 +0700

    update exercises

---
+++ exercise3.js
@@ -0,0 +1,17 @@
+//EXERCISE 5
+var nama = "Indra"
+var peran = "Tabib"
+
+peran = "Penyihir"
+nama = "Alia"
+
+if (peran === "Ksatria") {
+  console.log("Halo Ksatria " 
+  + nama + ", kamu dapat menyerang dengan senjatamu!")
+} else if (peran === "Tabib") {
+  console.log("Halo Tabib "+ nama +", kamu akan membantu temanmu yang terluka.")
+} else if (peran === "Penyihir") {
+  console.log("Halo Penyihir " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!")
+} else {
+  console.log("Pilih peranmu untuk memulai game!")
+} 
--- exercise7.js
+++ exercise7.js
@@ -1,3 +1,20 @@
 //EXERCISE 7
 
 //Question 1
+var rows1;
+for (var rows1=1; rows1 <= 5;rows1++ ){
+    console.log("*")
+}
+
+//Question 2
+var rows2; 
+var cols2;
+
+for (var rows2 = 1; rows2 <=5; rows2++){
+    console.log("*")
+    for (var cols2=0; cols2 <= 5; cols2++){
+        console.log("*");
+    } 
+} 
+
+//Question 3
