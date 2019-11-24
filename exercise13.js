e041566b1ff57ed9676bba669d771debd5672d82
aliamarsha <aliamarsha96@gmail.com>
2019-11-24 20:08:08 +0700

    add last exercise

--- exercise13.js
+++ exercise13.js
@@ -14,12 +14,15 @@
 } */
 
 function xo(str) {
-    var newStr
-    for (var i =0; i < str.length; i++){
-        console.log(newStr[i])
-    }
-  }
+    var x = 0;
+    var o = 0; 
 
+    for (var i = 0; i < str.length; i++){
+        if ( str[i] === "x") x++; 
+        if ( str[i] === "o") o++;
+    }
+        return x === o;
+}
   
   // TEST CASES
   console.log(xo('xoxoxo')); // true
